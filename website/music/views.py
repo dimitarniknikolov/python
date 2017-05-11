1a5fd54744845b3ad8e496af57136ef7f1d2504e
Dimitar Nikolov <dimitar.nikolov@mentormate.com>
2017-05-11 17:54:49 +0300

    show albums and song details

--- website/music/views.py
+++ website/music/views.py
@@ -1,4 +1,17 @@
+from django.http import Http404
 from django.http import HttpResponse
+from .models import Album
+from django.shortcuts import render
+
 
 def index(request):
-    return HttpResponse("<h1>This is the Music app homepage</h1> ")
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums': all_albums,})
+
+
+def detail(request, album_id):
+    try:
+        album = Album.objects.get(id=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album})
